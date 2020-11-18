54f493ee8a073937e6685981a1fb1e219aee4daa
parisaetemadinejad <parisaetemadinejad@gmail.com>
2020-11-18 18:18:43 +0330

    18:16 parisa commited event file

--- event.py
+++ event.py
@@ -1,3 +1,17 @@
+class Event:
+    def __init__(self, id_event, name_event, date_event, time_event, place_event, cost_event, total_capacity,
+                 mod_total_capacity, flag_event=1):
+        self.id_event = id_event
+        self.name_event = name_event
+        self.date_event = date_event
+        self.time_event = time_event
+        self.place_event=place_event
+        self.cost_event=cost_event
+        self.total_capacity = total_capacity
+        self.mod_total_capacity = mod_total_capacity
+        self.flag_event = flag_event
+    def create_event(self):
+
 # this library is for work by csv file
 import csv
 # this library is for import correct date and time to csv file
