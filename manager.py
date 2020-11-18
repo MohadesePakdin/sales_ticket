028d657cbd60b9e501a273a8f7832ed4aebe11d5
parisaetemadinejad <parisaetemadinejad@gmail.com>
2020-11-19 01:05:22 +0330

    parisa commit manager.py and add log in method and change manager.py

--- manager.py
+++ manager.py
@@ -11,7 +11,67 @@ from termcolor import colored
 
 
 class Admin:
-    def __init__(self):
+    def __init__(self, username, password,flag):
+        self.username = username
+        self.password = password
+        self.flag = flag
+    def login(self):
+        number_try = 3
+        while number_try > 0:  # baraye inke agar eshtebah vared kard dobare azash bkhad ke user pass bzne ya inke bre biron
+            print("input your username and password")
+            username = input("please enter your username: ")
+            password = input("please enter your password: ")
+            df_admin = pd.read_csv("admin.csv")
+            df_admin_indexed = df_admin.set_index("id_admin", drop=True)
+            try:
+                if str(df_admin_indexed.iloc[
+                           df_admin_indexed.index[df_admin_indexed['username'] == username].tolist()[
+                               0] - 1, 1]) == password:
+                    # obj_admin = Admin()
+                    while True:
+                        print("please select one of follow choices:\n")
+                        print("1-show all active event \n"
+                              "2-show all event \n"
+                              "3-show all deactivate event\n"
+                              "4-create event \n"
+                              "4-remove event \n"
+                              "5-Exit ")
+                        try:
+                            admin_input_selected = int(input("enter your choice: "))
+                            if admin_input_selected in range(1, 6):
+                                if admin_input_selected == 1:
+                                    print("show_all_active_event()")
+                                    Admin.show_event(Admin, 5)
+                                    # obj_admin.show_all_active_event()
+                                elif admin_input_selected == 2:
+                                    print("show_all_event()")
+                                    # obj_admin.show_all_event()
+                                elif admin_input_selected == 3:
+                                    print("show_all_deactive_event()")
+                                    # obj_admin.show_all_deactive_event()
+                                elif admin_input_selected == 4:
+                                    print("create_event()")
+                                    # obj_admin.create_event()
+                                elif admin_input_selected == 5:
+                                    print("exit()")
+                                    sys.exit()
+                            else:
+                                print("your input is not valid please select other choice")
+                        except ValueError:
+                            print("your input is not valid please select other choice")
+                    break
+            except IndexError:
+                if number_try > 1:
+                    print("your username or password is wrong please try again")
+                    number_try -= 1
+                else:
+                    print("your username or password is wrong")
+                    number_try -= 1
+        else:
+            input("\n\nyou try upper than 3 times your account block print any key to exit: ")
+            sys.exit()
+
+    def signup(self):
         pass
 
     def add_event(self):
