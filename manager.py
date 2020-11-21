438c12144ab533fa23789878b792748ea4cb4683
MohadesePakdin <mohi44pakdin@gmail.com>
2020-11-21 15:01:32 +0330

    ...

--- manager.py
+++ manager.py
@@ -16,7 +16,9 @@ import pandas as pd
 from termcolor import colored
 
 from person import Person
-
+import logging
+# #creat and configure logger
+logger = logging.getLogger()
 
 class Admin(Person):
     def __init__(self, status, username=None, password=None, flag=1):
@@ -27,7 +29,35 @@ class Admin(Person):
 
 
     def signup(self):
-        pass
+        file_path = "account.csv"# admin.csv
+        try:
+            df= pd.read_csv(file_path)
+            df_indexed = df.set_index("id_account", drop=True)# id_admin
+            # df_account = pd.read_csv("account.csv")
+            # df_account_indexed = df_account.set_index("id_account", drop=True)
+            list_username = list(df_indexed["username"])
+            while True:
+                username_account = input("input your username: ")
+                if username_account not in list_username[1:]:
+                    print("Your username is created then submit your password . ")
+                    break
+                else:
+                    print("Your name has already been entered with this username TRY ANOTHER ONE . ")
+            # username_account = input("Please input username: ")
+            password_account = input("Please input password: ")
+            print("Please select one of follow choice:")
+            print("You are logging in as : \n1-STUDENT \n2-EMPLOYEE \n3-TEACHER \n4-OTHER")
+
+            row_account = [[df_indexed.index[-1] + 1, username_account, str(password_account), type_account, 1]]
+
+            with open(file_path, 'a', newline='') as csv_account:
+                csv_writer = csv.writer(csv_account)
+                # writing the data row
+                csv_writer.writerows(row_account)
+        except Exception:
+            print("you have not this file please create a file with name account.csv and set first row with this items "
+                  "(id_account,username,password,flag)and second row with this item (0,) without parenthesis")
+            # logging.exception('not completely header in file')
 
     def add_event(self):
         # we have a csv file at first and csv file have 2 static record that dont delete this first row is my attribute
@@ -49,6 +79,7 @@ class Admin(Person):
                     break
                 except ValueError:
                     print("your date event is not valid please input with this format: 1399/08/26")
+                    logger.error("not valid")
             while True:
                 try:
                     time_event = str(datetime.strptime(input('Enter time of event with this format 20:00 : '),
@@ -72,26 +103,31 @@ class Admin(Person):
 
 
     def remove_event(self, df_first=None):
+        #Which event do you want to delete
         id_remove=int(input("please enter row for delete: "))
 
         while True:
             try:
-
+                #read csv file
                 df_first = pd.read_csv("event.csv")
+                #access  a specific few rows/columns from DataFrame
                 df_first.loc[df_first["id_event"]==id_remove,"Flag_event"]=0
+                #Write object to a (csv) file
                 df_first.to_csv("event.csv", index=False)
+                logger.info("event %s is removed." % id_remove)
 
             except EOFError:
-                print("File is empty. You must add a film first before you can remove it.")
-
+                # raised when a built-in function  do not read any data before encountering
+                print("File is empty. You must add a event first before you can remove it.")
+                logger.error("File is empty")
             except KeyError:
+                #raised when you try to access a key that dose not exist
                 print("That event doesn't exist.")
+                logger.error("doesn't exist")
 
             df = pd.read_csv(r"event.csv")
             print(df)
-
-
-
+            #Do you want to continue deleting or not
             keepLooping = input("\nAnother evet to delete? y/n: ")
             if keepLooping == "n":
                 break
@@ -99,8 +135,7 @@ class Admin(Person):
 
 
     def add_discount(self):
-        # we have a csv file at first and csv file have 2 static record that dont delete this first row is my attribute
-        # and second row is set first id and add them in future record
+        # we have a csv file at first and csv file have 2 static record
         # load csv file
         file_name = "discount.csv"
         # exception handling
@@ -108,7 +143,7 @@ class Admin(Person):
             df_discount = pd.read_csv(file_name)
             # set id_event for index in csv file
             df_discount_indexed = df_discount.set_index("id_discount", drop=True)
-            # get info of one event from admin and add to csv file
+            # get info of discount
             name_discount = input("Enter name of discount: ")
             # this while is for that we input correct answer to dataset
 
@@ -122,8 +157,36 @@ class Admin(Person):
                   "row with this items (id_event,Name_event,Date_event,Time_event,"
                   "place_event,Cost_event,Total_capacity,Mod_total_capacity,Flag_event)"
                   "and second row with this item (0,) without parenthesis ")
+            logger.error("file not fount")
 
 
+    def active_evevnt(self):
+        #Show current events
+        #open csv file
+        with open("event.csv", "r") as csv_file:
+            csv_reader = csv.DictReader(csv_file, delimiter=',')
+            #In this loop we want to display events whose flag is 1
+            for lines in csv_reader:
+                obj_event = Event(lines['id_event'], lines['Name_event'],lines['Date_event'],lines['Time_event'],
+                    lines['place_event'],lines['Cost_event'],lines['Total_capacity'],lines['Mod_total_capacity'],
+                    lines['Flag_event'])
+                if obj_event.flag_event == 1:
+                    print(obj_event)
+
+
+    def deactive_event(self):
+        #
+        # open csv file
+        with open("event.csv", "r") as csv_file:
+            csv_reader = csv.DictReader(csv_file, delimiter=',')
+            # In this loop we want to display events whose flag is 1
+            for lines in csv_reader:
+                obj_event = Event(lines['id_event'], lines['Name_event'], lines['Date_event'], lines['Time_event'],
+                                  lines['place_event'], lines['Cost_event'], lines['Total_capacity'],
+                                  lines['Mod_total_capacity'],
+                                  lines['Flag_event'])
+                if obj_event.flag_event == 0:
+                    print(obj_event)
 
 
 obj_user = Admin()
